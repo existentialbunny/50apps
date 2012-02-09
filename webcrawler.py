37a893b6b84fb67d461f7461c1cb56bb6fb5bf89
N W <fionabunny@gmail.com>
2012-02-08 21:45:42 -0800

    foo

--- webcrawler.py
+++ webcrawler.py
@@ -1,38 +1,50 @@
 
+'''
+Crawl function will start from a seed link looking for queries
+
+Seed - starts with a one term dictionary where the key is the 0th depth
+    and the value is the starting site
+
+depth - is the max depth to crawl, which also serves to rank per level
+
+query - is a search string of words separated by space characters
+'''
 def Crawl(linklist={}, depth=2, query="python"):
-	import urllib
-	import re
+    import urllib
+    import re
+    query = re.sub('\s+', '|',query)
+    
+    visitedlist = set()
+    for i in linklist:
+        visitedlist = visitedlist.union(set(linklist[i]))
 
-	visitedlist = set()
-	for i in linklist:
-		visitedlist = visitedlist.union(set(linklist[i]))
+    for i in range(depth):
+        if i in linklist.keys():
+            sitesperdepth=set(linklist[i])
+        else:
+            break
+        for thissite in sitesperdepth:
+            if thissite not in visitedlist or i == 0:
+                try:
+                    site = urllib.urlopen(thissite)
+                    visitedlist.add(site)
+                    contents = site.read()
+                    rank = re.findall(query, contents,re.IGNORECASE)
+                    if len(rank) > 0:
+                        print("{0}, Search Rank: {1}, Seed Rank: {2}".format(thissite, len(rank), i))
 
-	for i in range(depth):
-		if i in linklist.keys():
-			sitesperdepth=linklist[i]
-		else:
-			break
-		for thissite in sitesperdepth:
-			if thissite not in visitedlist or i == 0:
-				try:
-					site = urllib.urlopen(thissite)
-					visitedlist.add(site)
-					contents = site.read()
-					if re.findall(query, contents,re.IGNORECASE):
-						print(thissite)
-
-						links = re.findall(r"\<a href=[\'\"](http[s]*://[^\"\']+)[\"\']\>", contents)
-						
-						if (i + 1) in linklist.keys():
-							linklist[i + 1].extend(links)
-						else:
-							linklist[i + 1] = links
-						
-					site.close()
-				except Exception:
-					print("FAILED OPENING: ", thissite)
-		
+                        links = re.findall(r"\<a href=[\'\"](http://[^\"\']+)[\"\']\>", contents)
+                        
+                        if (i + 1) in linklist.keys():
+                            linklist[i + 1].extend(links)
+                        else:
+                            linklist[i + 1] = links
+                        
+                    site.close()
+                except Exception:
+                    print("BLACKLISTING: ", thissite)
+                    visitedlist.add(thissite)
+        
 if __name__ == '__main__':
-	Seed = {0:["http://news.ycombinator.com/"]} 
-	Seed[-1] = ["http://fish.sekure.us"]
-	Crawl(Seed, 5, "python")
+    Seed = {0:["http://news.ycombinator.com/"]} 
+    Crawl(Seed, 5, "python hacker")
